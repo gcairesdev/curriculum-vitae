1e567570f00b1aacec30bcd87c4b096fb70eebcd
Jefferson Eloy <jeffersoneloy08@gmail.com>
2020-03-22 15:07:08 -0300

    :art: Adiciona Bootstrap e Jquery

--- Views/pages/templates/header.php
+++ Views/pages/templates/header.php
@@ -2,10 +2,11 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title><?php echo self::titulo; ?></title>
-	<link rel="icon" type="image/png" href="<?php echo INCLUDE_PATH_FULL ?>img/favicon.ico">
+	<link rel="stylesheet" href="<?= INCLUDE_PATH_FULL ?>css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link href="<?php echo INCLUDE_PATH_FULL ?>css/reset.css" rel="stylesheet" type="text/css">
 	<link href="<?php echo INCLUDE_PATH_FULL ?>css/style.css" rel="stylesheet" type="text/css">
+	<link rel="icon" type="image/png" href="<?php echo INCLUDE_PATH_FULL ?>img/favicon.ico">
+	<title><?php echo self::titulo; ?></title>
 </head>
 <body>
 <header>
