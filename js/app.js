8b064a2919443d2ef48e5c6a6af9a5954c63202b
gcairesdev <pessoalgcaires@gmail.com>
2020-05-24 16:00:19 -0300

    :construction: Refatoração do App.js

--- js/app.js
+++ js/app.js
@@ -1,88 +1,55 @@
-// Função de mostrar preview
 $(document).ready(function(){
-   $('#pre-visualizar').click(function(){
+
+   $('#pre-visualizar').click(mostraPreview);
+   $('.preview-btn-close').click(escondePreview);
+   $('.next').click(proxPagina);
+   $('.prev').click(voltaPagina);
+   $('#adiciona-formacao').click(addFormacao);
+
+   function mostraPreview() {
       $('#preview').addClass('ative');
-   })
-});
+   }
 
-// Função de esconder preview
-$(document).ready(function(){
-   $('.preview-btn-close').click(function(){
+   function escondePreview() {
       $('#preview').removeClass('ative');
-   })
-});
+   }
 
-// Função do botão seguinte
-$(document).ready(function(){
-   $('.next').click(function(){
-      $('.form').find('section.ative').next('section').
-      addClass('ative').prev().
-      removeClass('ative');
-   })
-});
+   function proxPagina() {
+      $('.form')
+         .find('section.ative')
+         .removeClass('ative')
+         .next('section')
+         .addClass('ative');
+   }
 
-// Função do botão anterior
-$(document).ready(function(){
-   $('.prev').click(function(){
-      $('.form').find('section.ative').prev().
-      addClass('ative').next().
-      removeClass('ative');
-   })
-});
+   function voltaPagina() {
+      $('.form')
+         .find('section.ative')
+         .removeClass('ative')
+         .prev('section')
+         .addClass('ative');
+   }
 
-// Função para por mascaras nos inputs
-$(document).ready(function(){
-   $("#tel_residencial").mask('(99) 9999-9999');
-   $("#celular").mask('(99) 9 9999-9999');
-   $("#cep").mask('99.999-999');
-   
-   //Função para só permitir entrada de números no input.
-   $(document).on('keypress','.number',function(){
-      return event.charCode >= 48 && event.charCode <= 57;	
-   });
-   
-});
+   // To-do
+   (function mask() {
+      $("#tel_residencial").mask('(99) 9999-9999');
+      $("#celular").mask('(99) 9 9999-9999');
+      $("#cep").mask('99.999-999');
+      
+      $(document).on('keypress','.number',function(){
+         return event.charCode >= 48 && event.charCode <= 57;	
+      });
+   })();
 
+   function addFormacao() {
+      $('.formacao').clone().appendTo('#formacoes')
+      // $('#formacoes').append($('.formacao').html());
+   }
 
-//Função para adicionar uma nova formação acadêmica.
-$('#adiciona-formacao').click(function(e){
-
-   e.preventDefault();//Evita que o formulário seja enviado após clicar no botão.
-
-   var linha =    '<div class="formacao">'+
-                  '  <div class="formacao-body">'+
-                  '     <div class="form-group">'+
-                  '        <label for="curso">Curso</label>'+
-                  '        <input type="text" class="form-control" id="curso" name="curso[]">'+
-                  '     </div>'+   
-                  '     <div class="row">'+
-                  '        <div class="col">'+
-                  '           <div class="form-group">'+
-                  '              <label for="instituicao">Instituição</label>'+
-                  '              <input type="text" class="form-control" id="instituicao" name="instituicao[]">'+
-                  '           </div>'+
-                  '        </div>'+
-                  '        <div class="col">'+
-                  '           <div class="form-group">'+
-                  '              <label for="ano_conclusao">Ano de conclusão</label>'+
-                  '              <input type="text" class="form-control" name="ano_conclusao[]">'+
-                  '           </div>'+
-                  '        </div>'+
-                  '        <div class="col">'+
-                  '           <div class="form-group">'+
-                  '              <button id="remover-formacao" class="btn btn-secondary"><i class="fas fa-trash-alt"></i></button>'+
-                  '           </div>'+
-                  '        </div>'+
-                  '     </div>'+
-                  '  </div>'+
-                  '</div>';
-   
-   $('#formacoes').append(linha);//Adiciona nova linha como filha na div formações
-
-   $('#formacoes').on("click", "#remover-formacao", function(){//Função para remover a div adicionada
-      $(this).parent().parent().parent().parent().remove();//Sobe na árvore de elementos até chegar na div pai adicionada e a remove.
-   })
-});
+//    $('#formacoes').on("click", "#remover-formacao", function(){//Função para remover a div adicionada
+//       $(this).parent().parent().parent().parent().remove();//Sobe na árvore de elementos até chegar na div pai adicionada e a remove.
+//    })
+// });
 
 //Função para adicionar uma nova experiência.
 $('#adiciona-experiencia').click(function(e){
@@ -134,5 +101,7 @@ $('#adiciona-experiencia').click(function(e){
    $('#experiencias').on("click", "#remover-experiencia", function(){//Função para remover a div adicionada
       $(this).parent().parent().parent().parent().remove();//Sobe na árvore de elementos até chegar na div pai adicionada e a remove.
    })
+
+});
 
 });
